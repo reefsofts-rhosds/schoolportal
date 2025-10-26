f30d7a364f898e01284cfa5457f4d496aae8c262
reefsofts-rhosds <youri.lkh@girofle.org>
2025-10-26 17:28:09 +0100

    Add some docs

--- frontend/urls.py
+++ frontend/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('teacher/dashboard',teacher_view, name='Teacher Dashboard'),
-    path('pupil/dashboard',pupil_view, name='Pupil Dashboard'),  
-    path('',account_type_select, name='Choose Account Type' ), 
+    path('teacher/dashboard',teacher_view, name='Teacher Dashboard'), #  Define teacher dashboard
+    path('pupil/dashboard',pupil_view, name='Pupil Dashboard'),  # Define pupil dashboard
+    path('',account_type_select, name='Choose Account Type' ),  # Define Account type choice
 ]
